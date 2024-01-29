fa4d61cf0559f3bc5826526cb891681ba9f9d7ed
Sahar <sahar22798@gmail.com>
2024-01-29 07:52:01 +0200

    Added city and country schemas to see how foreign relations work using mySql

--- src/apps/web/db/schema.ts
+++ src/apps/web/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   int,
   mysqlEnum,
@@ -9,22 +10,32 @@ import {
 } from "drizzle-orm/mysql-core";
 
 // // declaring enum in database
-// export const countries = mysqlTable(
-//   "countries",
-//   {
-//     id: serial("id").primaryKey(),
-//     name: varchar("name", { length: 256 }),
-//   },
-//   (countries) => ({
-//     nameIndex: uniqueIndex("name_idx").on(countries.name),
-//   })
-// );
-// export const cities = mysqlTable("cities", {
-//   id: serial("id").primaryKey(),
-//   name: varchar("name", { length: 256 }),
-//   countryId: int("country_id").references(() => countries.id),
-//   popularity: mysqlEnum("popularity", ["unknown", "known", "popular"]),
-// });
+export const countries = mysqlTable(
+  "countries",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+  },
+  (countries) => ({
+    nameIndex: uniqueIndex("name_idx").on(countries.name),
+  })
+);
+export const cities = mysqlTable("cities", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  countryId: int("country_id"),
+  popularity: mysqlEnum("popularity", ["unknown", "known", "popular"]),
+});
+
+export const countryRelations = relations(countries, ({ many }) => ({
+  cities: many(cities),
+}));
+export const cityRelations = relations(cities, ({ one }) => ({
+  country: one(countries, {
+    fields: [cities.countryId],
+    references: [countries.id],
+  }),
+}));
 
 export const users = mysqlTable("users", {
   id: serial("id").primaryKey(),
--- src/apps/web/drizzle/0000_broad_captain_cross.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`full_name` text,
-	`phone` varchar(256),
-	CONSTRAINT `users_id` PRIMARY KEY(`id`)
-);
