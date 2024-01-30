3a6ef34201adcb5ea347ea64c4ba7cde2f12ab01
Sahar <sahar22798@gmail.com>
2024-01-30 12:41:40 +0200

    Commented out example tables

--- src/apps/web/db/schema.ts
+++ src/apps/web/db/schema.ts
@@ -9,33 +9,33 @@ import {
   text,
 } from "drizzle-orm/mysql-core";
 
-// // declaring enum in database
-export const countries = mysqlTable(
-  "countries",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-  },
-  (countries) => ({
-    nameIndex: uniqueIndex("name_idx").on(countries.name),
-  })
-);
-export const cities = mysqlTable("cities", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 256 }),
-  countryId: int("country_id"),
-  popularity: mysqlEnum("popularity", ["unknown", "known", "popular"]),
-});
+// // // declaring enum in database
+// export const countries = mysqlTable(
+//   "countries",
+//   {
+//     id: serial("id").primaryKey(),
+//     name: varchar("name", { length: 256 }),
+//   },
+//   (countries) => ({
+//     nameIndex: uniqueIndex("name_idx").on(countries.name),
+//   })
+// );
+// export const cities = mysqlTable("cities", {
+//   id: serial("id").primaryKey(),
+//   name: varchar("name", { length: 256 }),
+//   countryId: int("country_id"),
+//   popularity: mysqlEnum("popularity", ["unknown", "known", "popular"]),
+// });
 
-export const countryRelations = relations(countries, ({ many }) => ({
-  cities: many(cities),
-}));
-export const cityRelations = relations(cities, ({ one }) => ({
-  country: one(countries, {
-    fields: [cities.countryId],
-    references: [countries.id],
-  }),
-}));
+// export const countryRelations = relations(countries, ({ many }) => ({
+//   cities: many(cities),
+// }));
+// export const cityRelations = relations(cities, ({ one }) => ({
+//   country: one(countries, {
+//     fields: [cities.countryId],
+//     references: [countries.id],
+//   }),
+// }));
 
 export const users = mysqlTable("users", {
   id: serial("id").primaryKey(),
