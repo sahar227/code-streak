2123486a26955d271bf99f929b6e6860031292b5
Sahar <sahar22798@gmail.com>
2024-01-30 14:12:09 +0200

    Added schemas relevant to the project and updated mysql version on docker-compose file

--- src/apps/web/db/schema.ts
+++ src/apps/web/db/schema.ts
@@ -1,4 +1,4 @@
-import { relations } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   int,
   mysqlEnum,
@@ -7,38 +7,60 @@ import {
   varchar,
   serial,
   text,
+  datetime,
+  date,
 } from "drizzle-orm/mysql-core";
-
-// // // declaring enum in database
-// export const countries = mysqlTable(
-//   "countries",
-//   {
-//     id: serial("id").primaryKey(),
-//     name: varchar("name", { length: 256 }),
-//   },
-//   (countries) => ({
-//     nameIndex: uniqueIndex("name_idx").on(countries.name),
-//   })
-// );
-// export const cities = mysqlTable("cities", {
-//   id: serial("id").primaryKey(),
-//   name: varchar("name", { length: 256 }),
-//   countryId: int("country_id"),
-//   popularity: mysqlEnum("popularity", ["unknown", "known", "popular"]),
-// });
-
-// export const countryRelations = relations(countries, ({ many }) => ({
-//   cities: many(cities),
-// }));
-// export const cityRelations = relations(cities, ({ one }) => ({
-//   country: one(countries, {
-//     fields: [cities.countryId],
-//     references: [countries.id],
-//   }),
-// }));
 
 export const users = mysqlTable("users", {
   id: serial("id").primaryKey(),
-  fullName: text("full_name"),
-  phone: varchar("phone", { length: 256 }),
+  email: varchar("email", { length: 256 }).notNull().unique(),
+  userName: varchar("user_name", { length: 256 }).notNull().unique(),
+  gitHubId: varchar("github_id", { length: 256 }).notNull().unique(),
+  gitHubAccessToken: varchar("github_access_token", { length: 256 }),
 });
+
+export const userProfiles = mysqlTable("user_profiles", {
+  id: serial("id").primaryKey(),
+  userId: int("user_id").notNull().unique(),
+  // totalXp: int("total_xp"),
+  currentStreak: int("current_streak").default(0),
+  lastConsumedEventDate: datetime("last_consumed_event_date").default(
+    sql`CURRENT_TIMESTAMP`
+  ),
+});
+
+export const userDailyProgress = mysqlTable(
+  "user_daily_progress",
+  {
+    id: serial("id").primaryKey(),
+    userId: int("user_id").notNull(),
+    event: mysqlEnum("event", [
+      "github_commit",
+      "leetcode_submission",
+    ]).notNull(),
+    date: date("date").default(sql`(CURDATE())`),
+    // xp: int("xp"),
+    amount: int("amount").notNull(),
+  },
+  (userDailyProgress) => ({
+    uniqueIndex: uniqueIndex("user_id_date_event_idx").on(
+      userDailyProgress.userId,
+      userDailyProgress.date,
+      userDailyProgress.event
+    ),
+  })
+);
+
+export const userRelations = relations(users, ({ one }) => ({
+  userProfile: one(userProfiles, {
+    fields: [users.id],
+    references: [userProfiles.userId],
+  }),
+}));
+
+export const userProfilesRelations = relations(userProfiles, ({ one }) => ({
+  user: one(users, {
+    fields: [userProfiles.userId],
+    references: [users.id],
+  }),
+}));
