d4ee8064e3081c6cf6c83281f0aca6cd3c950d6e
Sahar <sahar22798@gmail.com>
2024-02-21 07:30:41 +0200

    Removed unneeded boilerplate from mobile

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,8 +1,4 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
-import { TestTW } from "@/components/TestTW";
+import { Text, View } from "react-native";
 import Auth, { Logout } from "@/components/Auth";
 import { useAtomValue } from "jotai";
 import { userAtom } from "../../state/auth";
@@ -10,35 +6,12 @@ import { userAtom } from "../../state/auth";
 export default function TabOneScreen() {
   const user = useAtomValue(userAtom);
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <TestTW />
+    <View>
+      <Text>Tab One</Text>
+      <View />
       {!user && <Auth />}
       {user && <Logout />}
       {user && <Text>Hello {user.name}</Text>}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,16 +1,9 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
 import * as SecureStore from "expo-secure-store";
-
-import { useColorScheme } from "@/components/useColorScheme";
 import { codeStreakApi } from "@/api";
 import { useAtomValue, useSetAtom } from "jotai";
 import { tokenExistsAtom, userAtom } from "../state/auth";
@@ -76,14 +69,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
   );
 }
