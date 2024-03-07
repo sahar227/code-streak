06ab780f964f50dbb882042a9431ae518459f705
Sahar <sahar22798@gmail.com>
2024-03-07 07:29:11 +0200

    small test refactor for better readability

--- apps/web/tests/unit/calculateNewUserStatus.test.ts
+++ apps/web/tests/unit/calculateNewUserStatus.test.ts
@@ -2,14 +2,16 @@ import { applyPushEvent, calculateNewUserStatus } from "@/features/syncCommits";
 import { UserStatusBuilder } from "./_testUtils/UserStatusBuilder";
 
 describe("applyPushEvent", () => {
-  it("should return the same user status if push was made before the last update", () => {
+  it("should return the same user status if we somhow process an event from the past", () => {
+    const pushedAt = new Date(2024, 3, 2, 10);
+    const streakExtendedAt = new Date(2024, 3, 3, 8); // We extended the streak later then the current push we are processing!
     const now = new Date(2024, 3, 3, 11);
 
     const userStatus = new UserStatusBuilder(undefined, now)
-      .withStreakExtendedAt(now)
+      .withStreakExtendedAt(streakExtendedAt)
       .build();
     const push = {
-      pushedAt: new Date(2024, 3, 2, 10).toISOString(),
+      pushedAt: pushedAt.toISOString(),
       commitMessages: [],
       repo: "test",
     };
@@ -18,17 +20,19 @@ describe("applyPushEvent", () => {
   });
 
   it("should increment the streak if push was made the day after the last push", () => {
+    const lastStreakExtension = new Date(2024, 3, 2, 8);
+    const pushedAt = new Date(2024, 3, 3, 10);
     const now = new Date(2024, 3, 3, 11);
 
     const userStatus = new UserStatusBuilder(undefined, now)
       .withLastUpdateDaysAgo(1)
-      .withStreakExtendedAt(new Date(2024, 3, 2, 8))
+      .withStreakExtendedAt(lastStreakExtension)
       .withCurrentStreak(1)
       .withLongestStreak(1)
       .build();
 
     const push = {
-      pushedAt: new Date(2024, 3, 3, 10).toISOString(),
+      pushedAt: pushedAt.toISOString(),
       commitMessages: [],
       repo: "test",
     };
@@ -45,17 +49,19 @@ describe("applyPushEvent", () => {
   });
 
   it("should NOT increment the streak if push was made the same day as the last push", () => {
+    const lastStreakExtension = new Date(2024, 3, 3, 8);
+    const pushedAt = new Date(2024, 3, 3, 10);
     const now = new Date(2024, 3, 3, 11);
 
     const userStatus = new UserStatusBuilder(undefined, now)
       .withLastUpdateDaysAgo(1)
-      .withStreakExtendedAt(new Date(2024, 3, 3, 8))
+      .withStreakExtendedAt(lastStreakExtension)
       .withCurrentStreak(1)
       .withLongestStreak(1)
       .build();
 
     const push = {
-      pushedAt: new Date(2024, 3, 3, 10).toISOString(),
+      pushedAt: pushedAt.toISOString(),
       commitMessages: [],
       repo: "test",
     };
